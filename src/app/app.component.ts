2cae456cea76832e5bb5a7527b4fec0b4888f661
Rafik Moreira <rafikmoreiradias@outlook.com>
2022-04-16 07:46:20 -0300

    module home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<router-outlet></router-outlet>',
+  styleUrls: ['../dist/scss/variables.scss'],
 })
 export class AppComponent {
   title = 'app-todo';
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,22 @@
+// Components
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItemsComponent } from './components/todo-input-add-items/todo-input-add-items.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+// Pages
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoListComponent,
+    TodoInputAddItemsComponent,
+    HomeComponent,
+  ],
+  imports: [CommonModule],
+})
+export class HomeModule {}
